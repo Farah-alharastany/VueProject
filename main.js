e3da6d2789df47b09bbce83a17ec296eee677b81
Farah-alharastany <Farah-alharastany@outlook.com>
2023-07-06 14:09:10 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            description:'its a product where every one can wear it with an affordable price',
+            hello: 'welcome farah',
+            nice: 'very beautiful'
+        }
+    } 
+})
